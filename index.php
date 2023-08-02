39e2475f5c5be37e9470d91c31a549e78a2f181e
simin <120188554+inho1019@users.noreply.github.com>
2023-08-02 15:38:27 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -119,8 +119,7 @@
         echo "text-align:center;";
     }
     ?>">
-        <legend style="text-align: left;font-weight : bold; font-size: 25px; border: 2px solid black;
-        padding:3px 5px 3px 5px; margin-top:3px; border-radius:5px; background-color:whitesmoke;">
+        <legend id='lege'>
             <?php
                 if(isset($_GET['mode'])) {
                     echo strtoupper($_GET['mode']);
