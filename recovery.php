b0078606e61607660fa1a6e15d0b5d8a3a96b43f
simin <120188554+inho1019@users.noreply.github.com>
2023-08-21 09:41:23 +0900

    Update recovery.php

--- recovery.php
+++ recovery.php
@@ -10,10 +10,10 @@ require('lib/function.php');
 global $con;
 $data = mysqli_fetch_assoc(mysqli_query($con,"select * from trash where num = ".$_GET['file']));
 if ($data['writer'] == $_SESSION['activate']['name'] || $_SESSION['activate']['id'] == 'admin') { 
-    echo "title : ".urldecode($data['writer'])."<br><br>";
+    echo "title : ".urldecode($data['title'])."<br><br>";
     echo "explain : ".str_replace("___",htmlspecialchars_decode("&nbsp;"),nl2br(htmlspecialchars(urldecode($data['exp']))));
 } else {
-    echo "title : ".$data['writer']."<br><br>";
+    echo "title : ".$data['title']."<br><br>";
     echo "explain : ".$data['exp'];
 }
 ?>
